f1c8bc211e90ea93dfd88c97ab3ce3c6b625c082
mrabdsaif <abdullah.saif11@yahoo.com>
2020-08-26 11:33:32 +0300

    JS updated

---
+++ js/script.js
@@ -0,0 +1,24 @@
+var name ="abdullah saif";
+var age = 23;
+var gender = "male";
+
+console.log("the name", name);
+console.log("the age", age);
+console.log("the gender", gender);
+
+
+
+if(age==23) {
+    alert("you have the same age");
+} else {
+    console.log("you are elder than him");
+}
+
+//prompt("what is your name")
+var gender1;
+prompt("what is your gender : ")
+console.log("user\`s gender", gender1)
+
+var name1= confirm("what is you name")
+console.log("user Name", name1)
+
